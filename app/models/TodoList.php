79e62f622208783d2a96f9d7575aba00d88fe4e0
Spenser Hale <spenserhale@outlook.com>
2014-11-17 00:18:43 -0800

    updated models to create relationship between list and item

--- app/models/TodoList.php
+++ app/models/TodoList.php
@@ -1,3 +1,14 @@
 <?php
 class TodoList extends Eloquent {
+
+	public function listItems()
+	{
+		return $this->hasMany('TodoItem');
+	}
+
+	public function delete()
+	{
+		TodoItem::where('todo_list_id', $this->id)->delete();
+		parent::delete();
+	}
 }

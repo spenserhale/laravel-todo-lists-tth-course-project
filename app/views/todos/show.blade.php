bf49a894b8d5fd72a54e936f83709f3ab61fc640
Spenser Hale <spenserhale@outlook.com>
2014-11-17 00:18:54 -0800

    created back button

--- app/views/todos/show.blade.php
+++ app/views/todos/show.blade.php
@@ -8,29 +8,7 @@
 			@else 
 				<h4>{{{ $item->content }}}</h4>
 			@endif
-			<ul class="no-bullet button-group ">
-				<li>
-					{{ link_to_route('todos.items.edit', 'edit', [$list->id, $item->id], ['class' => 'tiny button'])}}
-				</li>
-				@if ($item->completed_on === NULL)
-					<li>
-						{{ Form::model($item, ['route' => ['todos.items.complete', $list->id, $item->id], 'method' => 'patch' ]) }}
-							{{ Form::button('complete', ['type' => 'submit', 'class' => 'tiny success button'])}}
-						{{ Form::close() }}
-					</li>
-				@endif
-				<li>
-					{{ Form::model($item, ['route' => ['todos.items.destroy', $list->id, $item->id], 'method' => 'delete' ]) }}
-						{{ Form::button('destroy', ['type' => 'submit', 'class' => 'tiny alert button'])}}
-					{{ Form::close() }}
-				</li>
-			</ul>
 		@endforeach
-		<ul class="no-bullet button-group ">
-			<li>
-				{{ link_to_route('todos.items.create', 'add new item', [$list->id], ['class' => 'button success'])}}
-			</li>
-		</ul>
-		<p>{{ link_to_route('todos.index', 'back', null, ['class' => 'button']) }}</p>
+		<p>{{ link_to_route('todos.index', 'back')}}</p>
 	</div>
 @stop
